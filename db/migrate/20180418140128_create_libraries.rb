9e35f67bb1ed97e9d511be8bb6f4649d0367a157
Mark Philip <markphilip2018@gmail.com>
2018-05-01 00:59:50 +0200

    not null

--- db/migrate/20180418140128_create_libraries.rb
+++ db/migrate/20180418140128_create_libraries.rb
@@ -1,9 +1,9 @@
 class CreateLibraries < ActiveRecord::Migration[5.2]
   def change
     create_table :libraries do |t|
-      t.string :name
-      t.string :location
-      t.string :phone
+      t.string :name, null: false
+      t.string :location, null: false
+      t.string :phone, null: false
       t.timestamps
     end
   end
